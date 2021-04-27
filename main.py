d08c0a35f85952274b440799039120c5c08b9574
A11Z16 <73792721+A11Z16@users.noreply.github.com>
2021-04-27 22:20:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 ali = "Ali"                # Burda Ali ismini tanımladım
 ahmet = "Ahmet"             #Burda Ahmet ismini tanımladım
 s = "sen"                   #Sen kelimesini çok kullandığımız için kısalttım.
-sn = "ne"                   #Burda ne kelimesini kısaltım.
+n = "ne"                   #Burda ne kelimesini kısaltım.
 
 (bir, iki, uc, dort, bes, alti, yedi, sekiz, dokuz) = (1, 2, 3, 4, 5, 6, 7, 8, 9)   #Burda sayılara bir değişken ekledim.
 cumle1 = "Merhaba {dayı}, nasılsın?".format(dayı=ali) #Burda 1.cümlemi yazdım.
 print(cumle1)                                         #Burda 1.cümlemin ekrana yazmasını sağladım
 cumle2 = "İyiyim {a}, sen nasılsın?".format(a=ahmet)
 print(cumle2)
-cumle3 = "Bende iyiyim,{sn} yapıyorsun?"
+cumle3 = "Bende iyiyim,{n} yapıyorsun?" .format(n=n)
 print(cumle3)
-cumle4 = "Oyun oynuyorum,{s} {sn} yapıyorsun?"
+cumle4 = "Oyun oynuyorum,{s} {n} yapıyorsun?" .format(s=s,n=n)
 print(cumle4)
 cumle5 = "Kod yazıyorum"
 print(cumle5)
